2418a1f7d1abd13f3a99a1e44fb60afe6347bb71
Ivan V Penaloza <99299172+VPIvan@users.noreply.github.com>
2022-10-23 23:49:14 -0700

    Updated Roomba code
    changed motor speed, and added delay while turning to give enough time to change direction.

--- Roomba_Robotics.ino
+++ Roomba_Robotics.ino
@@ -1,87 +1,88 @@
-//UltraSonic Sensor 
 #include <NewPing.h>
-
-#define TRIGGER_PIN  7
-#define ECHO_PIN     6
+#define TRIGGER_PIN 7
+#define ECHO_PIN 6
 #define MAX_DISTANCE 200
 
 #define CW 0
 #define CCW 1
-//Motor Definitions
+
+// Motor Definitions
 #define MOTOR_A 0
 #define MOTOR_B 1
 
-// Pin Assignments
-// Don’t change these! These pins are statically defined by the shield layout 
-const byte PWMA = 3; // PWM control (speed) for motor A
-const byte PWMB = 11; // PWM control (speed) for motor B
-const byte DIRA = 12; // Direction control for motor A
-const byte DIRB = 13; // Direction control for motor B
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum // distance.
-void setup() {
-  setupArdumoto(); // Set all pins as outputs
-  }
-  
-void loop() {
-  delay(50);
-  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS). 
-  if ( uS / US_ROUNDTRIP_CM > 50 || uS / US_ROUNDTRIP_CM == 0)
-{
-// Move forward
-forward(150);
-}
-else if (uS / US_ROUNDTRIP_CM < 50)
-{
-turnRight(100);
-delay(500);
-}
-}
+//Pin assignments
+//do not alter pin numbers
 
-// driveArdumoto drives ’motor’ in direction ’dir’ at speed ’spd’ 
+//Ardumoto
+const byte PWMA = 3;  //PWM control (speed) for motor A
+const byte PWMB = 11; //PWM control (speed) for motor B
+const byte DIRA = 12; //Direction control for motor A
+const byte DIRB = 13; //Direction control for motor B
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup()
+{
+  setupArdumoto();  //Set all pins as outputs and initialize them to LOW
+  }
+void loop()
+{
+  delay(50);
+  unsigned int uS = sonar.ping();
+  if( uS / US_ROUNDTRIP_CM > 25 || uS / US_ROUNDTRIP_CM == 0)
+  {
+    // Move forward
+    forward(220);
+    }else if( uS / US_ROUNDTRIP_CM < 25)
+    {
+      turnRight(200);
+      delay(900);
+      }
+  }
+
 void driveArdumoto(byte motor, byte dir, byte spd)
 {
-if (motor == MOTOR_A) 
-{
-  digitalWrite(DIRA, dir);
-  analogWrite(PWMA, spd); 
-}
-else if (motor == MOTOR_B)
-{
-  digitalWrite(DIRB, dir); analogWrite(PWMB, spd);
-} 
-}
+  if( motor == MOTOR_A)
+  {
+    digitalWrite(DIRA, dir);
+    analogWrite(PWMA, spd);
+    }else if( motor == MOTOR_B)
+    {
+      digitalWrite(DIRB, dir);
+      analogWrite(PWMB, spd);
+      }
+  }
 
-void forward(byte spd) // Runs both motors at speed ’spd’ 
+void forward( byte spd)
 {
-  driveArdumoto(MOTOR_A, CW, spd); 
-  driveArdumoto(MOTOR_B, CW, spd);
-}
-
-void turnRight(byte spd) 
-{
-  stopArdumoto(MOTOR_B); 
   driveArdumoto(MOTOR_A, CW, spd);
-// stopArdumoto makes a motor stop 
-}
+  driveArdumoto(MOTOR_B, CW, spd);
+  }
 
-void stopArdumoto(byte motor)
+void turnRight( byte spd)
 {
-  driveArdumoto(motor, 0, 0); 
-}
- 
+  stopArdumoto(MOTOR_B);
+  driveArdumoto(MOTOR_A, CW, spd);
+  }
 
-// setupArdumoto initializes all pins 
+void stopArdumoto( byte motor)
+{
+  driveArdumoto(motor, 0, 0);
+  }
+
+
+// set up ardumoto initialize all the pins
 void setupArdumoto()
 {
-// All pins should be set up as outputs: 
-pinMode(PWMA, OUTPUT);
-pinMode(PWMB, OUTPUT);
-pinMode(DIRA, OUTPUT);
-pinMode(DIRB, OUTPUT);
+  //pins set as outputs
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
 
-// Initialize all pins as low: 
-digitalWrite(PWMA, LOW); 
-digitalWrite(PWMB, LOW); 
-digitalWrite(DIRA, LOW); 
-digitalWrite(DIRB, LOW);
-}
+  //initialize pins as low
+  pinMode(PWMA, LOW);
+  pinMode(PWMB, LOW);
+  pinMode(DIRA, LOW);
+  pinMode(DIRB, LOW);
+  }
